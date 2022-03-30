0bfd16173f0b3dffc622c226ca0a663cb062820d
Nickolai <mailtilatloggeind@protonmail.com>
2022-03-30 22:23:40 +0200

    hej

--- src/com/PrimsProjekt/AdjacencyListGraph.java
+++ src/com/PrimsProjekt/AdjacencyListGraph.java
@@ -6,13 +6,60 @@ public class AdjacencyListGraph {
 //Denne class er opbygget ud fra vejledningen i videoen "AdjacencyGraph" der ligger på kursets moodle side
     private ArrayList<Vertex> vertices;
 
+    public  AdjacencyListGraph(){
+        vertices = new ArrayList<Vertex>();
+    }
 
+    public void addVertex(Vertex v) {
+        vertices.add(v);
+    }
+
+    public void newEdge(Vertex from, Vertex to, Integer dist) {
+        if ( !(vertices.contains(from) && vertices.contains(to))) { //fjern parantes omkring den sidste vertices hvis det ikke virker
+            System.out.println(" Vertex not found");
+            return;
+        }
+        Edge newedge = new Edge(from, to, dist);
+    }
+
+    public void printGraph(){
+        Vertex currentv;
+        for (int i = 0 ; i < vertices.size() ; i++) {
+            currentv = vertices.get(i); //så vi ikke skal lave vertices.get alt for meget
+            System.out.println(" Edges from Vertex: " + currentv.getName());
+            for (int j = 0; j < currentv.getOutEdges().size(); j++) {
+                Edge currente = currentv.getOutEdges().get(j);
+                System.out.println("to: " + currente.getToVertex().getName() + " weight: " + currente.getWeight());
+            }
+            System.out.println(" ");
+        }
+    }
 }
 
 class Vertex implements Comparable<Vertex> { //Så vi kan sortere
     private String Name;
     private ArrayList<Edge> outEdges;
     Integer distance = Integer.MAX_VALUE;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public ArrayList<Edge> getOutEdges() {
+        return outEdges;
+    }
+
+    public void setOutEdges(ArrayList<Edge> outEdges) {
+        this.outEdges = outEdges;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
 
     public Vertex(String id) {
         this.Name = id;
@@ -32,10 +79,39 @@ class Vertex implements Comparable<Vertex> { //Så vi kan sortere
         else
             return 0;
     }
+
+    public String getName() {
+        return Name;
+    }
 }
 
 class Edge {
     private Vertex fromVertex;
+
+    public Vertex getFromVertex() {
+        return fromVertex;
+    }
+
+    public void setFromVertex(Vertex fromVertex) {
+        this.fromVertex = fromVertex;
+    }
+
+    public Vertex getToVertex() {
+        return toVertex;
+    }
+
+    public void setToVertex(Vertex toVertex) {
+        this.toVertex = toVertex;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
     private Vertex toVertex;
     private Integer weight;
 
@@ -43,6 +119,7 @@ class Edge {
         fromVertex = from;
         toVertex = to;
         weight = cost;
+        from.addOutEdge(this);
 
     }
 }
--- src/com/PrimsProjekt/Main.java
+++ src/com/PrimsProjekt/Main.java
@@ -4,5 +4,44 @@ public class Main {
 
     public static void main(String[] args) {
 	// Adjacency Graph
+
+        //Lav vertices
+        AdjacencyListGraph adjgraph = new AdjacencyListGraph();
+        Vertex A = new Vertex("0");
+        Vertex B = new Vertex("1");
+        Vertex C = new Vertex("2");
+        Vertex D = new Vertex("3");
+        Vertex E = new Vertex("4");
+        Vertex F = new Vertex("5");
+        Vertex G = new Vertex("6");
+
+        adjgraph.addVertex(A);
+        adjgraph.addVertex(B);
+        adjgraph.addVertex(C);
+        adjgraph.addVertex(D);
+        adjgraph.addVertex(E);
+        adjgraph.addVertex(F);
+        adjgraph.addVertex(G);
+
+        //Lav edges
+        adjgraph.newEdge(A, B, 1);
+        adjgraph.newEdge(A, C, 5);
+        adjgraph.newEdge(A, E, 3);
+        adjgraph.newEdge(B, E, 1);
+        adjgraph.newEdge(B, F, 7);
+        adjgraph.newEdge(C, D, 1);
+        adjgraph.newEdge(D, E, 1);
+        adjgraph.newEdge(D, G, 1);
+
+
+        //A.addOutEdge(new Edge(A, B, 1));
+        //A.addOutEdge(new Edge(A, C, 5));
+        //A.addOutEdge(new Edge(A, E, 3));
+        //B.addOutEdge(new Edge(B, E, 1));
+        //B.addOutEdge(new Edge(B, F, 7));
+        //C.addOutEdge(new Edge(C, D, 1));
+        //D.addOutEdge(new Edge(D, E, 1));
+        //D.addOutEdge(new Edge(D, G, 1));
+
     }
 }
